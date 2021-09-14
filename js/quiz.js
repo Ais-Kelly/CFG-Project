1a6754bd72b9600b3460bd802ac1f96d829412f2
Ais-Kelly <x20174608@student.ncirl.ie>
2021-09-14 18:36:09 +0100

    draft code

--- js/quiz.js
+++ js/quiz.js
@@ -1,3 +1,92 @@
 function quiz(){
-    
+
 }
+
+
+How would your friends describe you to someone you've never met?
+Creative
+Playful
+Confident
+Simple
+Friendly
+Serious
+
+Which of these do you value most?
+Memories
+Freedom
+Fame
+Kindness
+Individuality
+Friendship
+
+Which of the following is your ideal date location?
+Museum
+Park 
+Arcade 
+Theme park 
+Rooftop bar 
+Cinema 
+
+Which of these movie genres is your favourite? 
+Romance
+Horror
+Fantasy
+Coming of age 
+Comedy
+Animated Disney 
+
+Which of these is your favourite quality?
+Your personality 
+Your cooking
+Your intelligence
+Your taste in music
+Your sense of humor 
+
+Which of these music genres do you enjoy most? 
+Folk 
+Hip-hop 
+Pop 
+Rock 
+Classical
+Indie 
+
+Which of these gifts would you like to receive the most? 
+Baked goods 
+Plane ticket 
+Personalised scavenger hunt 
+Signed copy of your favourite book 
+Bouquet of your favourite flowers 
+Brand new car 
+
+Where do you feel most at home? 
+In nature 
+Baking at home 
+Browsing records 
+Library
+Concert 
+Playing video games
+
+Which of these supernatural creatures would you rather be? 
+Fairy 
+Dragon 
+Vampire 
+Mermaid 
+Zombie 
+Alien 
+
+Which of these characterists do you find most annoying in other people? 
+Being boring 
+Being impatient 
+Being ignorant 
+Being dependent 
+Being a follower 
+Being defensive 
+
+What is your favourite type of weather? 
+Snow 
+Thunder and lightening 
+Clear and sunny 
+Rain 
+Cool and breezy 
+Fog 
+
